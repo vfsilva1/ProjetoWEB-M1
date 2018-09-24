079f52fc808f35e9e3c12670d90c2fe6881394c7
Vitor Ferreira da Silva <vfsilva5@gmail.com>
2018-09-24 10:05:59 -0300

    Retangulo pulando e andando

--- ProjetoWEB-M1-master/gameScript.js
+++ ProjetoWEB-M1-master/gameScript.js
@@ -6,23 +6,68 @@ $(function(){
         width : 50,
         height : 50,
         x : 500,
-        y : 430,
-        speedX : 1,
-        speedY : 1
+        y : 100,
+        speedX : 0,
+        speedY : 0,
+        jumping : true
+    };
+    
+    var controller = {
+        up : false,
+        right : false,
+        left : false,
+        keyListener : function(event) {
+            var key_state = (event.type == "keydown")?true:false;
+        
+            switch (event.keyCode) {
+                case 37: //left key
+                    controller.left = key_state;
+                    break;
+                case 38: //up key
+                    controller.up = key_state;
+                    break;
+                case 39:
+                    controller.right = key_state;
+                    break;
+            }
+            
+        }
     };
     
     //rodando infinitamente
     function loop(){
         window.requestAnimationFrame(loop, cnv);
-        //checa se a tecla foi apertada
-        window.addEventListener("keyup", checkKeyPress, false);
+        window.addEventListener("keydown", controller.keyListener);
+        window.addEventListener("keyup", controller.keyListener);
         update();
         render();
     }
     
     //atualização dos elementos do jogo
     function update(){
-       
+        if(controller.up && rectangle.jumping == false) {
+            rectangle.speedY -= 30;
+            rectangle.jumping = true;
+        }
+        
+        if(controller.left)
+            rectangle.speedX -= 0.5;
+        
+        if(controller.right)
+            rectangle.speedX += 0.5;
+        
+        rectangle.speedY += 1.5;
+        rectangle.x += rectangle.speedX;
+        rectangle.y += rectangle.speedY;
+        rectangle.speedY *= 0.9;
+        rectangle.speedX *= 0.9;
+        
+        if(rectangle.y > 430) {
+            rectangle.jumping = false;
+            rectangle.y = 430;
+            rectangle.speedY = 0;
+            console.log(rectangle.jumping);
+        }
     }
     
     //desenhar elementos do jogo na tela
@@ -39,13 +84,7 @@ $(function(){
         ctx.fillStyle = "black";
         ctx.fillRect(0, 480, 1000, 7);
     }
-    
-    function checkKeyPress(key) {
-        //seta para cima
-        if(key.keyCode == "38"){
-            rectangle.y -= 20;
-        }
-    }
+   
     //chamando para rodar infinitamente
     loop();
 });
